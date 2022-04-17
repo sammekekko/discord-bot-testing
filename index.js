7266f7963be3831875e6d46775f2a9ee2891af22
sammekekko <samuel.kekkonen@yahoo.com>
2022-04-17 18:50:26 +0200

    initialize github repository

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hej") {
+        message.reply("bog")
+    }
+})
+
+client.login(process.env.TOKEN)
