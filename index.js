968d91a651f7bd96cfa223793d9a5103d41e1ecf
Samuel Kekkonen <samuelkekkonen@outlook.com>
2022-06-19 15:48:53 +0200

    organized utilities

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const Discord = require("discord.js");
 const utilityHandler = require("./utilities/utilityHandler.js");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const fs = require("fs");
+// const { REST } = require("@discordjs/rest");
+// const { Routes } = require("discord-api-types/v9");
+// const fs = require("fs");
 const { Player } = require("discord-player");
 
 const TOKEN = process.env.TOKEN;
 
 const LOAD_SLASH = process.argv[2] == "load";
-
-const CLIENT_ID = "965284413757857792";
-const GUILD_ID = "802508650085744641";
 
 const client = new Discord.Client({
     intents: [
@@ -32,33 +29,34 @@ client.player = new Player(client, {
 
 // Initializing SlashCommandBuilder
 
-let commands = []
+// let commands = []
 
-const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
-for (const file of slashFiles) {
-    const slashcmd = require(`./slash/${file}`)
-    client.slashcommands.set(slashcmd.data.name, slashcmd)
-    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
-}
+// const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+// for (const file of slashFiles) {
+//     const slashcmd = require(`./slash/${file}`)
+//     client.slashcommands.set(slashcmd.data.name, slashcmd)
+//     if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+// }
 
 if (LOAD_SLASH) {
-    const rest = new REST({
-        version: "9"
-    }).setToken(TOKEN)
-    console.log("Deploying slash commands")
-    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-            body: commands
-        })
-        .then(() => {
-            console.log("Successfully loaded")
-            process.exit(0)
-        })
-        .catch((err) => {
-            if (err) {
-                console.log(err)
-                process.exit(1)
-            }
-        })
+    // const rest = new REST({
+    //     version: "9"
+    // }).setToken(TOKEN)
+    // console.log("Deploying slash commands")
+    // rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+    //         body: commands
+    //     })
+    //     .then(() => {
+    //         console.log("Successfully loaded")
+    //         process.exit(0)
+    //     })
+    //     .catch((err) => {
+    //         if (err) {
+    //             console.log(err)
+    //             process.exit(1)
+    //         }
+    //     })
+    utilityHandler.loadCommands(client);
 } else { 
     client.on("ready", () => {
         utilityHandler.readyEvents(client);
--- utilities/utilityHandler.js
+++ utilities/utilityHandler.js
@@ -1,4 +1,10 @@
 const eventListeners = require("./eventListeners.js");
+const { REST } = require("@discordjs/rest");
+const fs = require("fs");
+const { Routes } = require("discord-api-types/v9");
+
+const CLIENT_ID = "965284413757857792";
+const GUILD_ID = "802508650085744641";
 
 module.exports.ready = async (client) => {
     console.log("---------------------");
@@ -12,4 +18,33 @@ module.exports.ready = async (client) => {
 
 module.exports.readyEvents = async (client) => {
     eventListeners.registerEvents(client);
+}
+
+module.exports.loadCommands = async (client) => {
+    let commands = []
+
+    const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+    for (const file of slashFiles) {
+        const slashcmd = require(`./slash/${file}`)
+        client.slashcommands.set(slashcmd.data.name, slashcmd)
+        if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+    }
+
+    const rest = new REST({
+        version: "9"
+    }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands
+        })
+        .then(() => {
+            console.log("Successfully loaded")
+            process.exit(0)
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(err)
+                process.exit(1)
+            }
+        })
 }
