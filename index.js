79d37ce56501a9c83c09fdf8bfa9d52080fa525b
Samuel Kekkonen <samuelkekkonen@outlook.com>
2022-06-19 15:40:35 +0200

    Use the heroku config, instead of local config.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
-const dotenv = require("dotenv");
 const utilityHandler = require("./utilities/utilityHandler.js");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const fs = require("fs");
 const { Player } = require("discord-player");
 
-dotenv.config();
 const TOKEN = process.env.TOKEN;
 
 const LOAD_SLASH = process.argv[2] == "load";
